4700defe01d392766415d2246f53ea8baea88896
bhathiya <bhathiya@wso2.com>
2017-10-30 15:28:17 +0530

    Fixed some path issues in the bash script

--- bosh-release/deploy-all.sh
+++ bosh-release/deploy-all.sh
@@ -15,44 +15,45 @@ mysql_analytics_password="root"
 event_store_db="event_store_db"
 processed_data_db="processed_data_db"
 stats_db="stats_db"
-
+APIM_NAME=wso2am-2.1.0
+ANALYTICS_NAME=wso2am-analytics-2.1.0
 APIM_PACK=wso2am-2.1.0.*.zip
 ANALYTICS_PACK=wso2am-analytics-2.1.0.*.zip
 JDK=jdk-8u144-linux-x64.tar.gz
 MYSQL_DRIVER=mysql-connector-java-5.1.24-bin.jar
 
-if [ ! -f ../$APIM_PACK ]; then
-    echo -e "\e[32m>>  APIM 2.1.0 pack not found! \e[0m"
+if [ ! -f ../../$APIM_PACK ]; then
+    echo -e "\e[32m>> APIM 2.1.0 pack not found! \e[0m"
     exit 1
 fi
 
-if [ ! -f ../$ANALYTICS_PACK ]; then
-    echo -e "\e[32m>>  APIM Analytics 2.1.0 pack not found! \e[0m"
+if [ ! -f ../../$ANALYTICS_PACK ]; then
+    echo -e "\e[32m>> APIM Analytics 2.1.0 pack not found! \e[0m"
     exit 1
 fi
 
-if [ ! -f ../$JDK ]; then
-    echo -e "\e[32m>>  JDK distribution (jdk-8u144-linux-x64.tar.gz) not found! \e[0m"
+if [ ! -f ../../$JDK ]; then
+    echo -e "\e[32m>> JDK distribution (jdk-8u144-linux-x64.tar.gz) not found! \e[0m"
     exit 1
 fi
 
-if [ ! -f ../$MYSQL_DRIVER ]; then
-    echo -e "\e[32m>>  MySQL Driver (mysql-connector-java-5.1.24-bin.jar) not found! \e[0m"
+if [ ! -f ../../$MYSQL_DRIVER ]; then
+    echo -e "\e[32m>> MySQL Driver (mysql-connector-java-5.1.24-bin.jar) not found! \e[0m"
     exit 1
 fi
 
 if [ ! -x "$(command -v mysql)" ]; then
-    echo -e "\e[32m>>  Please install MySQL client. \e[0m"
+    echo -e "\e[32m>> Please install MySQL client. \e[0m"
     exit 1
 fi
 
 if [ ! -x "$(command -v git)" ]; then
-    echo -e "\e[32m>>  Please install Git client. \e[0m"
+    echo -e "\e[32m>> Please install Git client. \e[0m"
     exit 1
 fi
 
 if [ ! -x "$(command -v docker)" ]; then
-    echo -e "\e[32m>>  Please install Docker. \e[0m"
+    echo -e "\e[32m>> Please install Docker. \e[0m"
     exit 1
 fi
 
@@ -62,10 +63,13 @@ if [ "$1" == "--force" ]; then
 fi
 
 #going to parent directory
-cd ..
+cd ../../
 
 APIM_PACK=$(ls wso2am-2.1.0.*.zip)
 ANALYTICS_PACK=$(ls wso2am-analytics-2.1.0.*.zip)
+
+cp $APIM_PACK $APIM_NAME.zip
+cp $ANALYTICS_PACK $ANALYTICS_NAME.zip
 
 echo -e "\e[32m>> Pulling MySQL docker image... \e[0m"
 docker pull mysql/mysql-server:5.7
@@ -74,7 +78,7 @@ if ! nc -z $mysql_apim_host 3306; then
     echo -e "\e[32m>> Starting MySQL docker container... \e[0m"
     docker run -d --name mysql-5.7 -p 3306:3306 -e MYSQL_ROOT_HOST=% -e MYSQL_ROOT_PASSWORD=$mysql_apim_password mysql/mysql-server:5.7 && docker ps -a
 
-    echo -e "\e[32m>> Waiting MySQL to start on 3306... \e[0m"
+    echo -e "\e[32m>> Waiting for MySQL to start on 3306... \e[0m"
     while ! nc -z $mysql_apim_host 3306; do
         sleep 1
         printf "."
@@ -86,7 +90,7 @@ else
 fi
 
 if [ ! -d wso2am-2.1.0 ]; then
-    echo -e "\e[32m>>  Extracting APIM 2.1.0 database scripts... \e[0m"
+    echo -e "\e[32m>> Extracting APIM 2.1.0 database scripts... \e[0m"
     unzip -q $APIM_PACK
 fi
 
@@ -98,12 +102,12 @@ echo -e "\e[32m>> Creating tables... \e[0m"
 mysql -h $mysql_apim_host -u $mysql_apim_username -p$mysql_apim_password -e "USE "$am_db"; SOURCE wso2am-2.1.0/dbscripts/apimgt/mysql5.7.sql; USE "$um_db"; SOURCE wso2am-2.1.0/dbscripts/mysql5.7.sql; USE "$reg_db"; SOURCE wso2am-2.1.0/dbscripts/mysql5.7.sql;"
 
 if [ ! -d bosh-deployment ]; then
-    echo -e "\e[32m>>  Cloning https://github.com/cloudfoundry/bosh-deployment... \e[0m"
+    echo -e "\e[32m>> Cloning https://github.com/cloudfoundry/bosh-deployment... \e[0m"
     git clone https://github.com/cloudfoundry/bosh-deployment bosh-deployment
 fi
 
 if [ ! -d vbox ]; then
-    echo -e "\e[32m>>  Creating envionment dir... \e[0m"
+    echo -e "\e[32m>> Creating envionment dir... \e[0m"
     mkdir vbox
 fi
 
@@ -145,12 +149,12 @@ bosh -e 192.168.50.6 alias-env vbox --ca-cert <(bosh int vbox/creds.yml --path /
 echo -e "\e[32m>> Loging in... \e[0m"
 bosh -e vbox login --client=admin --client-secret=$(bosh int vbox/creds.yml --path /admin_password)
 
-cd wso2-apim-bosh-release/
+cd pivotal-cf-apim/bosh-release/ 
 echo -e "\e[32m>> Adding blobs... \e[0m"
-bosh -e vbox add-blob ../jdk-8u144-linux-x64.tar.gz oraclejdk/jdk-8u144-linux-x64.tar.gz
-bosh -e vbox add-blob ../mysql-connector-java-5.1.24-bin.jar mysqldriver/mysql-connector-java-5.1.24-bin.jar
-bosh -e vbox add-blob ../wso2am-2.1.0.zip wso2apim/wso2am-2.1.0.zip
-bosh -e vbox add-blob ../wso2am-analytics-2.1.0.zip wso2apim_analytics/wso2am-analytics-2.1.0.zip
+bosh -e vbox add-blob ../../jdk-8u144-linux-x64.tar.gz oraclejdk/jdk-8u144-linux-x64.tar.gz
+bosh -e vbox add-blob ../../mysql-connector-java-5.1.24-bin.jar mysqldriver/mysql-connector-java-5.1.24-bin.jar
+bosh -e vbox add-blob ../../wso2am-2.1.0.zip wso2apim/wso2am-2.1.0.zip
+bosh -e vbox add-blob ../../wso2am-analytics-2.1.0.zip wso2apim_analytics/wso2am-analytics-2.1.0.zip
 
 echo -e "\e[32m>> Uploading blobs... \e[0m"
 bosh -e vbox -n upload-blobs
@@ -161,7 +165,6 @@ bosh -e vbox create-release --force
 echo -e "\e[32m>> Uploading bosh release... \e[0m"
 bosh -e vbox upload-release
 
-cd ..
 if [ ! -f bosh-stemcell-3445.7-warden-boshlite-ubuntu-trusty-go_agent.tgz ]; then
     echo -e "\e[32m>> Stemcell does not exist! Downloading... \e[0m"
     wget https://s3.amazonaws.com/bosh-core-stemcells/warden/bosh-stemcell-3445.7-warden-boshlite-ubuntu-trusty-go_agent.tgz
@@ -170,7 +173,6 @@ fi
 echo -e "\e[32m>> Uploading Stemcell... \e[0m"
 bosh -e vbox upload-stemcell bosh-stemcell-3445.7-warden-boshlite-ubuntu-trusty-go_agent.tgz
 
-cd wso2-apim-bosh-release/
 echo -e "\e[32m>> Deploying bosh release... \e[0m"
 yes | bosh -e vbox -d wso2apim deploy wso2apim-manifest.yml
 
--- bosh-release/undeploy.sh
+++ bosh-release/undeploy.sh
@@ -1,26 +1,5 @@
 #!/bin/bash
 
-#APIM MySQL
-mysql_apim_host="172.17.0.1"
-mysql_apim_username="root"
-mysql_apim_password="root"
-am_db="am_db"
-um_db="um_db"
-reg_db="reg_db"
-
-#Analytics MySQL
-mysql_analytics_host="172.17.0.1"
-mysql_analytics_username="root"
-mysql_analytics_password="root"
-event_store_db="event_store_db"
-processed_data_db="processed_data_db"
-stats_db="stats_db"
-
-APIM_PACK=$(ls wso2am-2.1.0.*.zip)
-ANALYTICS_PACK=$(ls wso2am-analytics-2.1.0.*.zip)
-JDK=$(ls jdk-8u144-linux-x64.tar.gz)
-MYSQL_DRIVER=$(ls mysql-connector-java-5.1.24-bin.jar)
-
 if [ ! -x "$(command -v docker)" ]; then
     echo -e "\e[32m>>  Please install Docker. \e[0m"
     exit 1
@@ -30,6 +9,8 @@ if [ ! -x "$(command -v bosh)" ]; then
     echo -e "\e[32m>>  Please install Bosh CLI v2. \e[0m"
     exit 1
 fi
+
+cd ../../
 
 echo -e "\e[32m>> Killing MySQL docker container... \e[0m"
 docker rm $(docker stop mysql-5.7) && docker ps -a
@@ -49,3 +30,4 @@ bosh delete-env bosh-deployment/bosh.yml \
  -v internal_cidr=192.168.50.0/24 \
  -v outbound_network_name=NatNetwork
 
+cd pivotal-cf-apim/bosh-release/
